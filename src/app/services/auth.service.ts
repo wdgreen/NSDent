66775a2d1f62d454ba737b2d61a517fbacfce99c
Mickaël <mlaborde@fabriquenumerique.fr>
2018-11-05 12:01:51 +0100

    :fire: Suppression du configs.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-// servers Urls 
-import { Settings } from "~/app/services/settings";
-// Used for notification system
+// // servers Urls 
+// import { Settings } from "~/app/services/settings";
+// // Used for notification system
 import { Globals } from '~/app/services/globals';
 
 @Injectable()
@@ -13,24 +13,24 @@ export class AuthService {
 
     constructor(private http: HttpClient) { }
 
-    // First Login
-    loginCabinet(formulaire) {
-        return this.http.post<any>(Settings.urlCabinets, formulaire);
-    }
+    // // First Login
+    // loginCabinet(formulaire) {
+    //     return this.http.post<any>(Settings.urlCabinets, formulaire);
+    // }
 
-    // Second Login
-    loginPatient(formulaire) {
-        // Define Http Header
-        this.httpOptions = {
-            headers: new HttpHeaders({
-                'Content-Type': 'application/json',
-                'username': `${formulaire.codePatient}`,
-                'password': `${formulaire.motDePasse}`
-            })
-        };
-        // Request API             Url                     /OS                    /Token                Header: Username & Password
-        return this.http.get<any>(`${Settings.urlPatients}/${Globals.appareil.os}/${Globals.appareil.pushToken}/`, this.httpOptions);
-    }
+    // // Second Login
+    // loginPatient(formulaire) {
+    //     // Define Http Header
+    //     this.httpOptions = {
+    //         headers: new HttpHeaders({
+    //             'Content-Type': 'application/json',
+    //             'username': `${formulaire.codePatient}`,
+    //             'password': `${formulaire.motDePasse}`
+    //         })
+    //     };
+    //     // Request API             Url                     /OS                    /Token                Header: Username & Password
+    //     return this.http.get<any>(`${Settings.urlPatients}/${Globals.appareil.os}/${Globals.appareil.pushToken}/`, this.httpOptions);
+    // }
 
     // logoutUser() {
     //     // localStorage.removeItem('token')
